4ae35896162146593b5663d7122ee43804fd68e3
hsb9291 <hsb9291@gmail.com>
2021-02-06 02:55:51 -0800

    removed prettier lint script

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function reverse(string) {
   const arr = string.split("");
   const revarr = arr.reverse();
   const revstring = revarr.join("");
-  return revstring; 
+  return revstring;
 }
 module.exports = reverse;
